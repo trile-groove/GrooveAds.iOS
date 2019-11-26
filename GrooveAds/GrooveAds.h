47a2c8b1083d9bf75ce067dfbdd1889dcb168402
Troy Lee <tri.le@groovetechnology.com>
2019-11-26 10:25:17 +0700

    Init

---
+++ GrooveAds/GrooveAds.h
@@ -0,0 +1,19 @@
+//
+//  GrooveAds.h
+//  GrooveAds
+//
+//  Created by Test on 11/26/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GrooveAds.
+FOUNDATION_EXPORT double GrooveAdsVersionNumber;
+
+//! Project version string for GrooveAds.
+FOUNDATION_EXPORT const unsigned char GrooveAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrooveAds/PublicHeader.h>
+
+
